763a6ecadab924aa485e020ec597aab1f3109c00
quintonlucyk <quinton_lucyk@yahoo.com>
2019-10-30 11:18:50 -0400

    ok

---
+++ code-compare/src/firebaseConfig.js
@@ -0,0 +1,11 @@
+const config = {
+  apiKey: "AIzaSyBvbqAt5GOz0srvOYmcTUJkyY35fLwPuEM",
+  authDomain: "code-compare-a1659.firebaseapp.com",
+  databaseURL: "https://code-compare-a1659.firebaseio.com",
+  projectId: "code-compare-a1659",
+  storageBucket: "code-compare-a1659.appspot.com",
+  messagingSenderId: "1047212355111",
+  appId: "1:1047212355111:web:7e8999c084ed284d766802"
+};
+
+export default config;
